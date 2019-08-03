6bd722285d7063fb8acf5a8ab16fa6495a5e694a
Klysman <klysman.rk@gmail.com>
2019-08-02 23:55:34 -0300

    Up

---
+++ Listas Exercicios.py
@@ -0,0 +1,23 @@
+""" #078
+maior = menor = 0
+lista = list()
+for i in range(0, 5):
+    lista.append(int(input('Digite um numero: ')))
+    if i == 0:
+        maior = menor = lista[i]
+    else:
+        if lista[i] > maior:
+            maior = lista[i]
+        if lista[i] < menor:
+            menor = lista[i]
+
+for pos, m in enumerate(lista):
+    if m == maior:
+        print(f'{pos}...')
+    elif m == menor:
+        print(f'{pos}...')
+        
+maior = max(lista)
+posicao = lista.index(maior)
+
+print(f'O menor valor é: {menor} e o maior valor é {maior} e está na posição {posicao}') """
