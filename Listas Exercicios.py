c95525afbe29a7c168312410b8aa02d2575de481
Klysman <klysman.rk@gmail.com>
2019-08-05 22:25:13 -0300

    Up

--- Listas Exercicios.py
+++ Listas Exercicios.py
@@ -132,3 +132,31 @@ if len(lista) == 0:
     print('Sua expressão está correta')
 else:
     print('Tente novamente') """
+
+
+#084
+
+pessoas = list()
+lista = list()
+
+for i in range(0, 3):
+
+
+    pessoas.append(str(input('Insira o nome:')))
+    pessoas.append(int(input('Insira o peso:')))
+    lista.append(pessoas[:])
+    pessoas.clear()
+
+
+pesados = list()
+magros = list()
+
+for peso in lista:
+    if peso[1] >= 80:
+        pesados.append(peso)
+    elif peso[1] < 80:
+        magros.append(peso)
+
+print(len(lista))
+print(pesados)
+print(magros)
