c458f1275904aaed83e8156b616cb2db45b571a2
Klysman <klysman.rk@gmail.com>
2019-08-06 23:07:08 -0300

    Up

--- Listas Exercicios.py
+++ Listas Exercicios.py
@@ -134,7 +134,7 @@ else:
     print('Tente novamente') """
 
 
-#084
+""" #084
 
 pessoas = list()
 lista = list()
@@ -159,4 +159,23 @@ for peso in lista:
 
 print(len(lista))
 print(pesados)
-print(magros)
+print(magros) """
+
+
+""" #085
+lista = [[],[]]
+
+for c in range(0, 7):
+    n = int(input(f'Digite o {c} numero: '))
+
+    if n % 2 == 0:
+        lista[0].append(n)
+
+    elif n % 2 == 1:
+        lista[1].append(n)
+
+lista[0].sort()
+lista[1].sort()
+
+print(lista[0])
+print(lista[1]) """
