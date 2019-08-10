7e6757a30b5c1b63eceb91782494fa1fa75f3929
Klysman <klysman.rk@gmail.com>
2019-08-10 13:18:57 -0300

    Update Listas Exercicios.py

--- Listas Exercicios.py
+++ Listas Exercicios.py
@@ -179,3 +179,14 @@ lista[1].sort()
 
 print(lista[0])
 print(lista[1]) """
+
+
+""" #085 Matriz
+matriz = [[],[],[]]
+for i in range(0, 3):
+    for j in range(0, 3):
+        n = int(input(f'Digite o valor para posição [{i},{j}]: '))
+        matriz[i].append(n)
+for c in range(0, 3):
+    for l in matriz[c]:
+        print(f'[{l:^5}]', end='') """
