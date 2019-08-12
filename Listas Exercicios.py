7641fc28a92d06cd6274aa0089ad5a87ca151527
Klysman <klysman.rk@gmail.com>
2019-08-11 21:26:55 -0300

    Up

--- Listas Exercicios.py
+++ Listas Exercicios.py
@@ -190,3 +190,36 @@ for i in range(0, 3):
 for c in range(0, 3):
     for l in matriz[c]:
         print(f'[{l:^5}]', end='') """
+
+#086
+
+matriz = [[],[],[]]
+for i in range(0,3):
+    for j in range(0, 3):
+        n = int(input(f'Insira a posição [{i},{j}]: '))
+        matriz[i].append(n)
+
+soma = 0
+soma2 = 0
+maior = menor = 0
+
+for c in range(0, 3):
+    for l in matriz[c]:
+        print(f'[{l:^5}]', end=' ')
+        
+        if l % 2 == 0:
+            soma += l
+
+    if c == 1:
+        maior = menor = matriz[1][c]
+    elif matriz[1][c] > maior:
+        maior = matriz[1][c]
+
+    soma2 += matriz[c][2]
+
+    print()
+
+
+print(soma)
+print(soma2)
+print(maior)
