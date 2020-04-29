eee255285ae2df15fa34b8d13c72a5de07f41d10
Klysman Rezende <klysman.rk@gmail.com>
2020-04-28 21:28:16 -0300

    up

--- Dicionários/Armazenando dados aleatorios em dicionarios.py
+++ Dicionários/Armazenando dados aleatorios em dicionarios.py
@@ -1,16 +1,32 @@
 from time import sleep
 from random import randint
+from operator import itemgetter
 
 dicionario = dict()
+lista = list()
 
-for c in range (0, 4):
+""" for c in range (0, 4):
     
-    dicionario['jogador'] = str(input('Qual o nome do jogador'))
+    dicionario['jogador'] = str(input('Qual o nome do jogador: '))
     dicionario['dado'] = randint(1,6)
+    lista.append(dicionario.copy()) """
 
+jogo = {'jogador1': randint(1,6),
+        'jogador2': randint(1,6),
+        'jogador3': randint(1,6),
+        'jogador4': randint(1,6)}
 
-for k, v in dicionario.items():
-    print(f' {k} = {v}')
+ranking = list()
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print(ranking) 
+
+for i, v in enumerate(ranking):
+    print(f'{i+1}º lugar: {v[0]} com {v[1]}')
+
+""" print(lista)
+for i in lista:
+    for k, v in i.items():
+        print(f' {k} = {v}') """
 
 
 
