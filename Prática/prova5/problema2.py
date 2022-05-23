899b6b187b6571e4c1a96dfe69c72d478e41534b
Klysman Rezende <klysman.rk@gmail.com>
2022-05-23 17:43:37 +0000

    up

--- Prática/prova5/problema2.py
+++ Prática/prova5/problema2.py
@@ -3,12 +3,17 @@
 def combine_letters():
     string1 = input('Digite a primeira string: ')
     string2 = input('Digite a segunda string: ')
+    new = ''
     
-    if string1 > string2:
+    if string1 == string2:
         for i in range(len(string1)):
-            print(string1[i] + string2[i])
-    else:
+            new = new + string1[i] + string2[i]
+    elif string1 < string2:
         for i in range(len(string2)):
-            print(string1[i] + string2[i])
-
+            new = new + string1[i] + string2[i]
+    else:
+        print ('erro')
+        
+    print (f'Combinação:{new}')
+    
 combine_letters()
