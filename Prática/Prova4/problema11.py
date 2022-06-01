294d50dd141fd096c5444d4a04185249ac37b453
Klysman Rezende <klysman.rk@gmail.com>
2022-06-01 16:15:37 +0000

    up

--- Prática/Prova4/problema11.py
+++
@@ -1,7 +0,0 @@
-divisores = 0 
-n = int(input('digite um valor: '))
-for i in range(1,n+1):
-    if n%i == 0:
-        divisores += i
-divisores = divisores - n
-print(f'Resultado: {divisores}') 
--- Prática/Prova4/problema22.py
+++
@@ -1,9 +0,0 @@
-def serie_harmonica(n):
-    soma = 0.0
-    for den in range(1,n+1):
-        soma = soma + (1.0/den)
-        
-
-    return soma
-
-print(serie_harmonica(6))
