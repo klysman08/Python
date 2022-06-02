31b3d3ac39a77d6cd4d68cd206412b4e924f2d95
Klysman <klysman.rk@gmail.com>
2022-06-02 18:24:37 -0300

    up

--- Prática/Lista6/problema1.py
+++ Prática/Lista6/problema1.py
@@ -1,14 +1,21 @@
 
-ip = list()
+ip = str(input(f'Digite um valor da parte: '))
 
-for i in range(0, 4):
-    valor = int(input(f'Digite um valor da parte {i}: '))
-    ip.append(valor)
-        
-        
-for j in ip:
-    if j > 255:
-        print ('O valor digitado não é válido')
-        
-print(f'O IP digitado foi {ip[0]}.{ip[1]}.{ip[2]}.{ip[3]}')
-    
+new_ip = ip.split('.')
+
+results = list(map(int, new_ip))
+
+
+count = 0
+
+for j in results:
+    if j < 0 or j > 255:
+        print ('Inválido')
+        break
+    else:
+        count += 1
+
+if count == 4:
+    print ('Válido')
+
+""" print(f'O IP digitado foi {ip[0]}.{ip[1]}.{ip[2]}.{ip[3]}') """
