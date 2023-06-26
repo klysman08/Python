146091c9c6c204a383022ababb1bff6768952ff0
Klysman08 <klysman.rk@gmail.com>
2023-06-26 11:46:51 +0100

    test application multvision

---
+++ Tests/df2matrix.py
@@ -0,0 +1,33 @@
+""" Input The input argument is a DataFrame object with five columns: FirstName LastName GradYear and MedianScore. 
+The data frame represents the mean and median test scores for various students (where LastNa and GradYear columns represent the first name, last name, 
+and graduation year o each contains integers in the set [2021, 2022, 2023].
+
+Output Your df2matrix function should retum a NumPy array containing the numeric representation of the data stored in [PÊ]. Specifically, the NumPy array will have the same number of rows as [PÊ] and five columns. • The rows are not sorted in this initial order must not change. 
+• The first column represents the values, while the second column represents the MedianScore values. • The final three columns are indicator columns for the GradYear column, where the third column represents year 2021, the fourth column represents year 2022, and the fifth column represents year 2023. """
+
+
+import numpy as np
+import pandas as pd
+
+def df2matrix(df):
+    """
+    Converts a DataFrame object to a NumPy array with the specified format.
+
+    Parameters:
+    df (pandas.DataFrame): A DataFrame object with five columns: FirstName, LastName,
+                           GradYear, and MedianScore.
+
+    Returns:
+    numpy.ndarray: A NumPy array containing the numeric representation of the data
+                   stored in df with five columns.
+    """
+    # Create an indicator matrix for the GradYear column
+    indicator_matrix = pd.get_dummies(df['GradYear'])
+
+    # Combine the MedianScore column and the indicator matrix into a single DataFrame
+    data_matrix = pd.concat([df['MedianScore'], indicator_matrix], axis=1)
+
+    # Convert the DataFrame to a NumPy array
+    np_array = data_matrix.to_numpy()
+
+    return np_array
---
+++ Tests/standardize_date.py
@@ -0,0 +1,35 @@
+""" Input The input argument (arr) is a 2-D NumPy array containing floating-point numbers. An example input would look like this: """
+
+""" Output Your standardize data function will return arr with each of its rows standardized. Specifically, for a row of values: """
+
+import numpy as np
+
+def standardize_data(arr):
+    """
+    Standardizes each row of a 2-D NumPy array.
+
+    Parameters:
+    arr (numpy.ndarray): A 2-D NumPy array containing floating-point numbers.
+
+    Returns:
+    numpy.ndarray: The input array with each of its rows standardized.
+    """
+    # Calculate the mean and standard deviation of each row
+    row_means = np.mean(arr, axis=1)
+    row_stdevs = np.std(arr, axis=1)
+
+    # Subtract the mean and divide by the standard deviation for each row
+    standardized_arr = (arr - row_means[:, np.newaxis]) / row_stdevs[:, np.newaxis]
+
+    return standardized_arr
+
+""" Test with arr = np.array([
+    [1.2, -0.3, 1.3],
+    [1.2, 2., 0.3]
+]).astype(np.float64)
+ """
+arr = np.array([
+    [1.2, -0.3, 1.3],
+    [1.2, 2., 0.3]
+]).astype(np.float64)
+print(standardize_data(arr))
