e58bf575efa2a990d91602636d31fe267a61482f
Klysman08 <klysman.rk@gmail.com>
2023-06-26 01:05:33 +0100

    new file:   map_reduce_lambdas/lambdas.py

--- map_reduce_lambdas/lambdas.py
+++ map_reduce_lambdas/lambdas.py
@@ -1,5 +1,6 @@
 from functools import reduce
 
+# List of students with their names and grades
 alunos = [
     {'nome': 'João', 'nota': 5.0},
     {'nome': 'Maria', 'nota': 7.0},
@@ -8,7 +9,11 @@ alunos = [
     {'nome': 'Pedro', 'nota': 10.0}
 ]
 
+# Lambda function to check if a student's grade is greater than or equal to 7.0
 aluno_aprovado = lambda aluno: aluno['nota'] >= 7.0
 
+# Filter the list of students to only include those who passed
 alunos_aprovados = filter(aluno_aprovado, alunos)
+
+# Print the list of students who passed
 print(list(alunos_aprovados))
